351a552ca6fff6ba88fbce54add0bfa7b2f11d49
a-programmer <mrsadin@gmail.com>
2022-10-16 17:44:16 +0330

    Pagination component improvements

--- KSBlazor.Components/Paging/Pagination.razor.cs
+++ KSBlazor.Components/Paging/Pagination.razor.cs
@@ -11,6 +11,12 @@ public class PagingInfo
     public int TotalItems { get; set; }
     public int PageSize { get; set; }
     public int CurrentPage { get; set; }
+    public string NextButtonCssClass { get; set; }
+    public string PrevButtonCssClass { get; set; }
+    public string? SearchTerm { get; set; }
+    public string? OrderByProperty { get; set; }
+    public bool ShowPagination { get; set; }
+    public bool? Descending { get; set; }
     public int TotalPages
     {
         get
